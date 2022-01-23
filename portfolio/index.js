3f5250d7296ad8e4b9d3bf52de207f1c3fc07e7d
gur90 <annettgur@gmail.com>
2022-01-24 00:57:39 +0300

    feat: add media for 620px and 320px

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,30 +1,24 @@
-console.log("1.Вёрстка валидная  +10 \n 2. Вёрстка семантическая +20 \n 3. Вёрстка соответствует макету  но есть отклонения по пикселям, особенно в части footer +42 \n 4. Требования к css + 12 \n 5. Интерактивность, реализуемая через css +20 \n Итого: 104 баллa")
+console.log("часть 1. \n 1.Вёрстка валидная  +10 \n 2. Вёрстка семантическая +20 \n 3. Вёрстка соответствует макету  но есть отклонения по пикселям, особенно в части footer +42 \n 4. Требования к css + 12 \n 5. Интерактивность, реализуемая через css +20 \n Итого: 104 баллa") 
+console.log("часть 2 \n Ваша отметка - 67 балла(ов) \n Отзыв по пунктам ТЗ: \n Частично выполненные пункты: \n 1) блок header — 3 балл(а) \n 2) секция hero — 3 балл(а) \n 3) секция skills — 3 балл(а) \n 4) секция portfolio — 3 балл(а) \n 5) секция price — 3 балл(а) \n 6) блок footer — 3 балл(а) \n Все оставшиеся пункты выполнены и не имеют комментариев проверяющего.");
+
+
 
 const hamburger = document.querySelector('.hamburger');
-
 const nav = document.querySelector('.nav');
-const navList = document.querySelector('.nav-list');
 const navItem = document.querySelector('.nav-item');
-
-
-if (hamburger) {
-    function toggleMenu() {
-        document.body.classList.toggle('lock');
-        hamburger.classList.toggle('open');
-        nav.classList.toggle('active');
-        navList.classList.toggle('active');
-        navItem.classList.toggle('active');
-    }
+function toggleMenu() {
+  hamburger.classList.toggle('open');
+  nav.classList.toggle('active');
+  navItem.classList.toggle('active');
 }
 hamburger.addEventListener('click', toggleMenu);
 
-if (hamburger.classList.contains ('active')) {
-    function closeMenu() {
-        document.body.classList.remove('lock');
-        hamburger.classList.remove('active');
+function closeMenu() {
+     
+        hamburger.classList.remove('open');
         nav.classList.remove('active');
-        navList.classList.remove('active');
         navItem.classList.remove('active');
+       
     }
-};
-navItem.forEach(el => el.addEventListener('click', closeMenu));
+  
+  nav.addEventListener('click', closeMenu);
