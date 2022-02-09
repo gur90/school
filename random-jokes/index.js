61aec9be5005a19659dc77674f3152bb32758aaf
gur90 <annettgur@gmail.com>
2022-02-09 23:57:10 +0300

    feat: write js

---
+++ random-jokes/index.js
@@ -0,0 +1,28 @@
+const objText = {
+   
+}
+const text = document.querySelector('.text');
+const author = document.querySelector('.author');
+const button = document.querySelector('.button');
+async function getQuotes() {
+    const url = 'data.json';
+    const res = await fetch(url);
+    const data = await res.json();
+    
+    const randomNum = Math.floor(Math.random()*data.length);
+    const tex = data[randomNum].text;
+    const auth = data[randomNum].author;
+    text.innerHTML=tex;
+    author.innerHTML=auth;
+  }
+  getQuotes();
+  button.addEventListener('click', getQuotes);
+
+  var i = 0;
+  var imgs = new Array('./assets/img/1.jpg', './assets/img/2.jpg', './assets/img/3.jpg','./assets/img/4.jpg','./assets/img/5.jpg','./assets/img/6.jpg','./assets/img/7.jpg', './assets/img/8.jpg',)
+  
+  function changeImg() {
+      var image = document.querySelector('.img');
+      image.src=imgs[++i];
+  }
+  button.addEventListener('click',() => changeImg())
