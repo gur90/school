335f0467af8f4b706046cee718c34fd0ea7967fd
gur90 <annettgur@gmail.com>
2022-02-11 01:34:41 +0300

    fix: fix  en

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -10,34 +10,23 @@ console.log("Ваша отметка - 65 балла(ов)\n Частично в
 const text = document.querySelector('.text');
 const author = document.querySelector('.author');
 const button = document.querySelector('.button');
-async function getQuotes() {
-    const url = 'data.json';
+var data = {};
+async function getQuotes(url) {
     const res = await fetch(url);
-    const data = await res.json();
-    
-    const randomNum = Math.floor(Math.random()*data.length);
-    const tex = data[randomNum].text;
-    const auth = data[randomNum].author;
-    text.innerHTML=tex;
-    author.innerHTML=auth;
+    data = await res.json();
+    changeQuote ()
   }
-  getQuotes();
-  button.addEventListener('click', getQuotes);
-
-  /*async function getEnQuotes() {
-    const url = 'dataEn.json';
-    const res = await fetch(url);
-    const data = await res.json();
-    
-    const randomNum = Math.floor(Math.random()*data.length);
-    const tex = data[randomNum].text;
-    const auth = data[randomNum].author;
-    text.innerHTML=tex;
-    author.innerHTML=auth;
-  }
-  elementButtonEn = document.querySelector('.en')
-  elementButtonEn.addEventListener("click",getEnQuotes)*/
+  getQuotes("data.json");
   
+  button.addEventListener('click', changeQuote);
+    function changeQuote () {
+    const randomNum = Math.floor(Math.random()*data.length);
+    const tex = data[randomNum].text;
+    const auth = data[randomNum].author;
+    text.innerHTML=tex;
+    author.innerHTML=auth;
+}
+
   var i = 0;
   var imgs = new Array('./assets/img/1.jpg', './assets/img/2.jpg', './assets/img/3.jpg','./assets/img/4.jpg','./assets/img/5.jpg','./assets/img/6.jpg','./assets/img/7.jpg', './assets/img/8.jpg',)
   
@@ -52,7 +41,7 @@ async function getQuotes() {
   }
   button.addEventListener('click',() => changeImg())
 
-    elementButtonEn = document.querySelector('.en')
+elementButtonEn = document.querySelector('.en')
 function getTranslate(language) {
    var lang = document.querySelectorAll('[data-i18]');
   
@@ -60,21 +49,16 @@ function getTranslate(language) {
         
         el.textContent = objText[language][el.dataset.i18];
     });
-    
+    if (language == 'ru') {
+        getQuotes("data.json")
+    } if (language == 'en') {
+        getQuotes("dataEn.json")
+    }
 }
 elementButtonEn.addEventListener("click", ( ) => { getTranslate('en')})
 
-    elementButtonRu = document.querySelector('.ru')
-function getTranslate(language) {
-   var lang = document.querySelectorAll('[data-i18]');
-  
-    lang.forEach((el) => {
-        
-        el.textContent = objText[language][el.dataset.i18];
-       
-    })
-  
-}
+elementButtonRu = document.querySelector('.ru')
+
 elementButtonRu.addEventListener("click", ( ) => { getTranslate('ru')});
 
 const ruButton = document.querySelectorAll('.ru');
